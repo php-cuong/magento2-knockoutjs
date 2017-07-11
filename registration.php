01c03d774d768b242ec586e829f7a0880f4db47f
Cuong NQ <bestearnmoney87@gmail.com>
2017-07-11 22:52:56 +0700

    init source code

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @Author: Ngo Quang Cuong
+ * @Date:   2017-07-11 14:35:21
+ * @Last Modified by:   nquangcuong
+ * @Last Modified time: 2017-07-11 14:35:41
+ * @website: http://giaphugroup.com
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'PHPCuong_ProductQty',
+    __DIR__
+);
---
+++ view/frontend/web/js/view/qty.js
@@ -0,0 +1,61 @@
+/*
+* @Author: Ngo Quang Cuong
+* @Date:   2017-07-11 15:14:29
+* @Last Modified by:   nquangcuong
+* @Last Modified time: 2017-07-11 20:52:12
+*/
+
+define(
+    [
+        'jquery',
+        'ko',
+        'uiComponent'
+    ],
+    function($, ko, Component) {
+        'use strict';
+
+        return Component.extend({
+
+            defaults: {
+                template: 'PHPCuong_ProductQty/cart/item/configure/qty'
+            },
+
+            initialize: function() {
+                this._super();
+                this.qty = ko.observable(this.defaultQty);
+            },
+
+            decreaseQty: function() {
+                var newQty = parseInt(this.qty()) - 1;
+                if (newQty < 1) {
+                    newQty = 1;
+                }
+                this.qty(newQty);
+                $('input[name="qty"]').val(newQty);
+            },
+
+            increaseQty: function() {
+                var newQty = parseInt(this.qty()) + 1;
+                if (newQty > 100) {
+                    newQty = 100;
+                }
+                this.qty(newQty);
+                $('input[name="qty"]').val(newQty);
+            },
+
+            qtyChanged: function() {
+                var newQty = parseInt($('input[name="qty"]').val());
+                if (isNaN(newQty)) {
+                    $('input[name="qty"]').val('1');
+                    newQty = 1;
+                }
+                if (newQty > 100) {
+                    newQty = 100;
+                }
+                this.qty(parseInt(newQty));
+                $('input[name="qty"]').val(newQty);
+            }
+
+        });
+    }
+);
